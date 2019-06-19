7a29f0f380c65db94aab3f63981284ca0511bf85
Austin Carnahan <ac6404@gmail.com>
2019-06-19 14:58:00 -0500

    Initial commit

---
+++ etl/settings .example.py
@@ -0,0 +1,6 @@
+# Insert twitter api keys here and save as settings.py
+
+access_token = "YOUR_KEY_HERE"
+access_token_secret = "YOUR_KEY_HERE"
+consumer_key = "YOUR_KEY_HERE"
+consumer_secret = "YOUR_KEY_HERE"
